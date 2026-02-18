cd5827f13cc51c98acd6429b10d2d87b0f663062
enesbakkal <enesbakkal@gmail.com>
2026-02-19 00:00:04 +0300

    add Domain, Application, Infrastructure projects to solution

---
+++ MeetingRoom.Api/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeetingRoom.Api.Entities;
+
+namespace MeetingRoom.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Room> Rooms => Set<Room>();
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Reservation>()
+                .HasIndex(r => new { r.RoomId, r.StartTime, r.EndTime });
+        }
+    }
+}
---
+++ MeetingRoom.Api/Entities/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace MeetingRoom.Api.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public int Capacity { get; set; }
+        public int Floor { get; set; }
+
+        // Simple string for equipment list for now
+        public string? Equipment { get; set; }
+    }
+}
